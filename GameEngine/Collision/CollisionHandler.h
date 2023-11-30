12c1bc3ebc34ec9ba533d0e9f3e49c034c8c159a
Alen Forko <60032174+AlenForko@users.noreply.github.com>
2023-11-30 13:20:06 +0100

    CollisionHandler

---
+++ GameEngine/Collision/CollisionHandler.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+class CollisionHandler
+{
+    
+public:
+    static float DistanceSquared(float2 PositionA, float2 PositionB)
+    {
+        float deltaX = PositionB.x - PositionA.x;
+        float deltaY = PositionB.y - PositionA.y;
+        return (deltaX * deltaX) + (deltaY * deltaY);
+    }
+    
+    static bool Collided(float2 PositionA, float RadiusA, float2 PositionB, float RadiusB)
+    {
+        const float DistaneSquared = DistanceSquared(PositionA, PositionB);
+        const float RadiiSum = RadiusA + RadiusB;
+        
+        return DistaneSquared <= (RadiiSum * RadiiSum);
+    }
+};
