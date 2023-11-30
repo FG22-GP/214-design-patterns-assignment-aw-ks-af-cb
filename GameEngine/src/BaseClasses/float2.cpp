3510fd1407cb1a86ec8454bb9f086d02d3715106
ChristianBackstrom <christian.berggren.backstrom@gmail.com>
2023-11-30 13:35:06 +0100

    float2 added

---
+++ GameEngine/src/BaseClasses/float2.cpp
@@ -0,0 +1,13 @@
+﻿#include "float2.h"
+
+float2::float2()
+{
+    this->X = 0;
+    this->Y = 0;
+}
+
+float2::float2(float X, float y)
+{
+    this->X = X;
+    this->Y = y;
+}
---
+++ GameEngine/src/BaseClasses/float2.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+struct float2
+{
+public:
+    float X;
+    float Y;
+
+    float2();
+    float2(float X, float y);
+};
