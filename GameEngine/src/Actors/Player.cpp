55b212cce9d6afd9318840f66f02e040b9543625
Alen Forko <60032174+AlenForko@users.noreply.github.com>
2023-12-01 14:07:49 +0100

    Pushing player

---
+++ GameEngine/src/Actors/Player.cpp
@@ -0,0 +1,21 @@
+﻿#include "Player.h"
+
+Player::Player(float speed, float2 direction) : IVelocity(speed, direction)
+{
+    
+}
+
+void Player::Fire()
+{
+    
+}
+
+void Player::Aim()
+{
+    
+}
+
+void Player::Move(float2 input)
+{
+    
+}
