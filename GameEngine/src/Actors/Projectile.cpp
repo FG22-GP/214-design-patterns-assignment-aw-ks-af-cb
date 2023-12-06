efdb2f362c5dfe7fe14e468cb6d9259f50a98f2f
ChristianBackstrom <christian.berggren.backstrom@gmail.com>
2023-12-06 15:33:38 +0100

    Projectile moves by offsetting its position with direction * speed * deltaTime

--- GameEngine/src/Actors/Projectile.cpp
+++ GameEngine/src/Actors/Projectile.cpp
@@ -2,14 +2,12 @@
 
 #include "Projectile.h"
 
-#include <iostream>
 
 Projectile::Projectile(SDL_Rect* rect, const char filePath[], int collisionRadius, float speed, float2 direction): Actor(rect, filePath, collisionRadius) ,IVelocity(speed, direction) {}
 
 void Projectile::Update(float DeltaTime)
 {
-    // Rect->x += static_cast<int>(speed * direction.X * DeltaTime);
-    // Rect->y += static_cast<int>(speed * direction.Y * DeltaTime);
+    AddPositionOffset(direction * speed * DeltaTime);
 }
 
 void Projectile::RenderPass(SDL_Renderer* renderer)
@@ -24,8 +22,7 @@ void Projectile::Destroy()
 
 void Projectile::Reset()
 {
-    // Rect->x = 9999;
-    // Rect->y = 9999;
+    SetPosition(float2(10000, 10000));
     direction = {0, 0};
     speed = 0;
 }
