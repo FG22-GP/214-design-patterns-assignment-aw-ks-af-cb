8e25f2ae0a60c3b10891a41b97d5f978c54180d5
Alen Forko <60032174+AlenForko@users.noreply.github.com>
2023-12-12 16:22:17 +0100

    Highscore works now
    Saves a .txt file that keeps track of the score

--- GameEngine/src/BaseClasses/ScoreManager.cpp
+++ GameEngine/src/BaseClasses/ScoreManager.cpp
@@ -35,10 +35,13 @@ int ScoreManager::GetHighScore() const
 void ScoreManager::SaveHighScoreToFile(const std::string& filename) const
 {
     std::ofstream file(filename);
-    if (file.is_open()) {
+    if (file.is_open())
+    {
         file << std::to_string(Highscore);  // Convert int to string before writing
         file.close();
-    } else {
+    }
+    else
+    {
         // Handle the case where the file couldn't be opened for writing
         std::cerr << "Error saving high score to file: " << filename << std::endl;
     }
@@ -49,16 +52,21 @@ void ScoreManager::LoadHighScoreFromFile(const std::string& filename)
     std::ifstream file(filename);
     if (file.is_open()) {
         std::string highScoreStr;
-        if (std::getline(file, highScoreStr)) {
-            try {
+        if (std::getline(file, highScoreStr))
+        {
+            try
+            {
                 Highscore = std::stoi(highScoreStr);
-            } catch (const std::exception& e) {
-                // Handle the exception (e.g., invalid format of high score)
+            }
+            catch (const std::exception& e)
+            {
                 std::cout << "Error converting high score: " << e.what() << std::endl;
             }
         }
         file.close();
-    } else {
+    }
+    else
+    {
         // If the file doesn't exist, create it and set an initial high score
         SaveHighScoreToFile(filename);
     }
