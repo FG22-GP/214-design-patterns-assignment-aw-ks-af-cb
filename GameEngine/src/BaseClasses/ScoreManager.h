2d4135b2e7dd5b144583075bd7a575650d650051
Alen Forko <60032174+AlenForko@users.noreply.github.com>
2023-12-12 16:12:42 +0100

    Highscore manager

---
+++ GameEngine/src/BaseClasses/ScoreManager.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include <string>
+
+class ScoreManager
+{
+public:
+    ScoreManager();
+
+    void IncreaseScore(int score);
+
+    int GetCurrentScore() const;
+
+    void UpdateHighScore();
+
+    int GetHighScore() const;
+
+    void SaveHighScoreToFile(const std::string& filename) const;
+    
+    void LoadHighScoreFromFile(const std::string& filename);
+
+private:
+    int CurrentScore;
+    int Highscore;
+};
