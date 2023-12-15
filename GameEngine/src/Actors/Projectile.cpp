3ba0b1bd3b7843ca9d365f89e445a16fc0b93fa2
ChristianBackstrom <christian.berggren.backstrom@gmail.com>
2023-12-15 13:59:52 +0100

    added a small little trail to projectile

--- GameEngine/src/Actors/Projectile.cpp
+++ GameEngine/src/Actors/Projectile.cpp
@@ -31,6 +31,25 @@ void Projectile::Update(float DeltaTime)
 void Projectile::RenderPass(SDL_Renderer* renderer)
 {
     Actor::RenderPass(renderer);
+
+    
+    int startX = static_cast<int>(std::round(Position.X));
+    int startY = static_cast<int>(std::round(Position.Y));
+    int endX = static_cast<int>(std::round(Position.X + (-direction.X * 200 * Lifetime / 2)));
+    int endY = static_cast<int>(std::round(Position.Y + (-direction.Y * 200 * Lifetime / 2)));
+
+    SDL_SetRenderDrawColor(renderer, 215, 63, 49,255 / 2);
+    const int Size = 3;
+    for (int x = -Size/2; x < Size/2; ++x)
+    {
+        for (int y = -Size/2; y < Size / 2; ++y)
+        {
+            SDL_RenderDrawLine(renderer, startX + x * 3, startY + y * 3, endX + x * 3, endY + y * 3);
+        }
+    }
+    
+
+    SDL_SetRenderDrawColor(renderer, 55, 66, 89, 255);
 }
 
 void Projectile::Destroy()
